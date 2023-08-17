7fca6579f0b2ca4cf504a76c232ad3e752a6d11c
DESKTOP-JL8J59F\user1 <khbkhb85098509@gmail.com>
2023-08-17 12:28:10 +0900

    java first commit

---
+++ src/변수의스코프/VarScopeClass.java
@@ -0,0 +1,25 @@
+package 변수의스코프;
+// 변수가 할당되고 사용되는 범위 확인
+
+public class VarScopeClass {
+    public static void main(String[] args) {
+//        int value = 10; // int 형으로 선언된 value 변수에 10을 대입
+//        int sum = value + 20; // int 형으로 선언된 sum에 value의 값 + 20;
+//        System.out.println("SUM의 값 : " + sum);
+
+//        int n = 20;
+//        if (n > 10) {
+//            int m = 30;
+//            m = n - 10;
+//            System.out.println("m 값 : " +m);
+
+//        }
+        byte num1 = 127; // -128 ~ 127
+        num1++;
+
+        System.out.println(num1);
+
+
+
+    }
+}
---
+++ src/샘플프로그램/SampleClass.java
@@ -0,0 +1,14 @@
+package 샘플프로그램; // 패키지명 : 클래스의 묶음
+/*
+     Date : 2023.08.14
+     Author : H.B.Kim
+     Porpose : 샘플 프로그램 입니다.
+ */
+
+// 접근제한자이며 모두 허용
+public class SampleClass {
+    // 메소드에 대한 접근제한자 public, protected, private, 아무것도 없는 디폴트
+    public static void main(String[] args) {
+        System.out.println("아무거나 출력");
+    }
+}
